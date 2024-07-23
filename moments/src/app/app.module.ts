3e3c3942137847b31e511d547b28071a76a33600
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-07-23 15:31:55 -0300

    adicionando formulario

--- moments/src/app/app.module.ts
+++ moments/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
+import { MomentFormComponent } from './components/moment-form/moment-form.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { NewMomentComponent } from './components/pages/new-moment/new-moment.com
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    NewMomentComponent
+    NewMomentComponent,
+    MomentFormComponent
   ],
   imports: [
     BrowserModule,
