64cc3d360d1a3dc33a9c97ffe5d9ee0a2fe5d720
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-07-24 14:21:26 -0300

    validation forms

--- moments/src/app/components/moment-form/moment-form.component.ts
+++ moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
@@ -8,9 +9,31 @@ import { Component, OnInit, Input } from '@angular/core';
 export class MomentFormComponent implements OnInit {
   @Input() btnText!: string;
 
+  momentForm!: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('',[Validators.required]),
+      description: new FormControl('',[Validators.required]),
+      Image: new FormControl(''),
+    });
   }
 
+  get title(){
+    return this.momentForm.get('title')!;
+  }
+
+  get description(){
+    return this.momentForm.get('description')!;
+  }
+
+  submit(){
+    if(this.momentForm.invalid){
+      return;
+    }
+    console.log('enviou o formulario');
+  }
 }
