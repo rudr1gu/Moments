ca6cd96048d363a987c47c7714516a82306ded7e
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-07-24 13:39:24 -0300

    feat: add interface and services

---
+++ moments/src/app/Comment.ts
@@ -0,0 +1,8 @@
+export interface Comment {
+    id?: number;
+    text: string;
+    username: string;
+    created_at?: string;
+    updated_at?: string;
+    momentId?: number;
+};
--- moments/src/app/app.module.ts
+++ moments/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// Add the following import statement to the top of the file:
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +25,10 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
