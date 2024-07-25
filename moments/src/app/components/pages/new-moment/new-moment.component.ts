ba41a2baf7310a39a0ccd134bbea1acc6aae454b
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-07-25 13:51:16 -0300

    editando a página de about e terminando os metodos de mensagens

--- moments/src/app/components/pages/new-moment/new-moment.component.ts
+++ moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Moment } from 'src/app/Moments';
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-moment',
@@ -10,7 +12,10 @@ import { MomentService } from 'src/app/services/moment.service';
 export class NewMomentComponent implements OnInit {
   btnText = 'Criar Momento';
 
-  constructor(private momentService: MomentService) { }
+  constructor(
+    private momentService: MomentService,
+    private messagesServices: MessagesService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -23,10 +28,12 @@ export class NewMomentComponent implements OnInit {
     if(moment.image){
       formData.append('image', moment.image);
     }
-    // todo
+    
     await this.momentService.createMoment(formData).subscribe();
-    // exibir mensagem de sucesso
-    // redirecionar para a página de momentos
+
+    this.messagesServices.add('Momento criado com sucesso!');
+    
+    this.router.navigate(['/']);
   }
 
 }
--- moments/src/app/services/messages.service.ts
+++ moments/src/app/services/messages.service.ts
@@ -7,4 +7,16 @@ export class MessagesService {
   messages: string = '';
 
   constructor() { }
+
+  add(message: string) {
+    this.messages = message;
+
+    setTimeout(() => {
+      this.clear();
+    }, 4000);
+  }
+
+  clear() {
+    this.messages = '';
+  }
 }
